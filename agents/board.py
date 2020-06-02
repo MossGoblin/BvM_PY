6cf78392a63c3a8aed10335e6bc5c0fc655aabdb
Angel Slavchev <bbb99@eur.corp.vattenfall.com>
2020-06-02 13:52:50 +0000

    [v > foundation] Trying to fill in a sample board

--- agents/board.py
+++ agents/board.py
@@ -6,6 +6,7 @@ parentdir = os.path.dirname(currentdir)
 sys.path.insert(0,parentdir)
 
 import config
+import random
 
 class Board():
     def __init__(self):
@@ -25,32 +26,63 @@ class Board():
 
 
     def init_board(self):
+        available_cells = self.get_available_cells()
+        check = 0
+        # random selection check
+        next_random_cell_index = random.randrange(len(available_cells))
+        next_random_cell = available_cells[next_random_cell_index]
+
         # recursive
         # base case - nine pieces on the board
         # B starts first
-        latest_piece = config.EMPTY
-        available_cells = self.get_available_cells()
+        latest_piece = config.B_PIECE
+        bracket = config.B_PIECE + config.M_PIECE
+        # step:
+        # get available cells
+        # select and fill in a cell
+        # alternate pices
+        # check for base case
+        for counter in range(10):
+            available_cells = self.get_available_cells()
+            next_random_cell_index = random.randrange(len(available_cells))
+            next_random_cell = available_cells[next_random_cell_index]
+            config.BOARD_STATE[next_random_cell_index] = latest_piece
+            latest_piece = bracket - latest_piece
+
         check = 0
+
+
+
 
         #HERE
         return False
 
 
-    def check_line(self, line):
+    def get_count(self, state):
+        counter = 0
+        for cell in state:
+            if cell == 0:
+                counter += 1
+        return counter
+    
+    def check_line(self, line, state):
     # check if all cells in the line are of one type and not 0
     # return True if the line is a win, else return false
-        if (self.b_state[self.l_defs[line[0]]] == 
-            self.b_state[self.l_defs[line[1]]] == 
-            self.b_state[self.l_defs[line[2]]]) & (self.b_state[self.l_defs[line[0]] != 0]):
+        cell_a = state[self.l_defs[line][0]]
+        cell_b = state[self.l_defs[line][1]]
+        cell_c = state[self.l_defs[line][2]]
+        if (cell_a == 
+            cell_b == 
+            cell_c) & (cell_a != 0):
             return True
         return False
     
-    def check_all_lines(self):
+    def check_all_lines(self, state):
         # check if any of the lines is a win
         # return True if so
         # else return False
-        for line in range(9):
-            if (self.check_line(line) == True):
+        for line in range(8):
+            if (self.check_line(line, state) == True):
                 return True
         return False
 
@@ -59,10 +91,14 @@ class Board():
         # returns all cells that are available for placement
         # skips cells that can produce win
         availables = []
-        for cell in self.b_state:
+        index = -1
+        for cell in self.b_state: #
+            index += 1
             if (cell == 0):
-                if (self.check_cell(cell, 1) == False) & (self.check_cell(cell, 2) == False): # empty cell that can not produce a win
-                    availables.append(cell)
+                cell_one = self.check_cell(index, 1)
+                cell_two = self.check_cell(index, 2)
+                if (cell_one == False) & (cell_two == False): # empty cell that can not produce a win
+                    availables.append(index)
         return availables
 
     def check_cell(self, cell, agent):
@@ -74,21 +110,33 @@ class Board():
         # check if the cell is available at all
         if self.b_state[cell] != 0:
             print(f'checked cell {cell} for win, but it is occupied')
+            raise
 
-        # build a triplet from the board state with the agent value in the cell in question
-        for line in self.l_defs: # for each line
-            if cell in line: # if the line has the cell in question
-                counter = 0
-                triplet = []
-                for line_cell in line: # for each cell in the line
-                    if line_cell == cell:
-                        triplet.append(agent) # place the agent value on the cell in question
-                    else:
-                        triplet.append(self.b_state[cell]) # get the cell value from the board state
-                    counter += 1
+        # TODO change logic:
+        # add the agent mark
+        # then check all lines
+
+        # fill in the cell
+        check_state = config.BOARD_STATE.copy()
+        check_state[cell] = agent
+        # check for any win lines
+        result = self.check_all_lines(check_state)
+        return result
+
+        # # build a triplet from the board state with the agent value in the cell in question
+        # for line in self.l_defs: # for each line
+        #     if cell in line: # if the line has the cell in question
+        #         counter = 0
+        #         triplet = []
+        #         for line_cell in line: # for each cell in the line
+        #             if line_cell == cell:
+        #                 triplet.append(agent) # place the agent value on the cell in question
+        #             else:
+        #                 triplet.append(self.b_state[cell]) # get the cell value from the board state
+        #             counter += 1
         
-                # check the triplet for win
-                if (triplet[0] == triplet[1] == triplet[2]) & (triplet[0] != 0):
-                    return True # WTF - need to get out of the method
+        #         # check the triplet for win
+        #         if (triplet[0] == triplet[1] == triplet[2]) & (triplet[0] != 0):
+        #             return True # WTF - need to get out of the method
 
-        return False
+        # return False
